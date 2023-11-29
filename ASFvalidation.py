1bab970344a0cc60f22f68c01b522f9dd3324f3c
Christian <cheva141083@gmail.com>
2023-11-28 22:26:48 -0600

    Get file in excel and open it

--- ASFvalidation.py
+++ ASFvalidation.py
@@ -5,33 +5,28 @@ from openpyxl.styles import NamedStyle, Border, Side
 
 
 
-
-
-
-def contar_archivos_en_directorio(folder):
-    try:
-        # Lista de elementos en el directorio
-        elementos = os.listdir(folder)
-
-        # Contador para archivos
-        contador_archivos = 0
-
-        # Itera sobre los elementos y cuenta los archivos
-        for elemento in elementos:
-            ruta_completa = os.path.join(folder, elemento)
-            if os.path.isfile(ruta_completa):
-                contador_archivos += 1
-
-        return contador_archivos
-
-    except FileNotFoundError:
-        print(f'El directorio {folder} no existe.')
-    except Exception as e:
-        print(f'Ocurrió un error al contar los archivos: {e}')
-
 # Search a file in a specific directory
 folderUbication ='E:/Resources/NAM US/Python/QA ASF'
 
-# Obtiene y muestra la cantidad de archivos en el directorio
-cantidad_archivos = contar_archivos_en_directorio(folderUbication)
-print(f'La cantidad de archivos en el directorio {folderUbication} es: {cantidad_archivos}')
+#Get list of files in the folder
+filesOnFolder = os.listdir(folderUbication)
+
+#filter excel files
+excelFiles = [file for file in filesOnFolder if file.endswith('.xlsx')]
+
+#Check if there are excel files in the folder
+if excelFiles:
+    # Get the first file 
+    first_file = os.path.join(folderUbication, excelFiles[0])
+
+    # Open the excel file con OpenPyXL
+    book = load_workbook(first_file)
+
+    # Operations to apply in file
+    worksheet = book.active
+    print(worksheet.title)
+
+    # Cierra el libro después de trabajar con él
+    book.close()
+else:
+    print("There are not excel files in this folder")
